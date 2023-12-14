73eb2b07526f1236f525713dada5d3e99a5a67e1
gnoparus <surapong@gmail.com>
2023-12-14 15:54:11 +0700

    try removing resource_type

--- utilities.js
+++ utilities.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 console.log(cloudinary.url("food/my_favorite/cheesecake"));
-console.log(cloudinary.url("dog", {resource_type: "video"}));
+console.log(cloudinary.url("dog", { resource_type: "video" }));
+
+// // not found
+// console.log(cloudinary.url("dog"));
 
 console.log(cloudinary.image("food/my_favorite/cheesecake"));
 
